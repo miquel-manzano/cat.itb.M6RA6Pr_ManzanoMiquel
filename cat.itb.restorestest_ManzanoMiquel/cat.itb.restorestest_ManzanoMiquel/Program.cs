ce7588d3b29d4ae8af1d0644bc8c9785032ef8a3
Miquel Manzano <miquel.manzano.7e6@itb.cat>
2025-05-09 10:13:48 +0200

    Component Working >:3

--- cat.itb.restorestest_ManzanoMiquel/cat.itb.restorestest_ManzanoMiquel/Program.cs
+++ cat.itb.restorestest_ManzanoMiquel/cat.itb.restorestest_ManzanoMiquel/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace cat.itb.restorestest_ManzanoMiquel
+﻿using cat.itb.gestioHR.depDAO;
+using cat.itb.restore_ManzanoMiquel;
+
+namespace cat.itb.restorestest_ManzanoMiquel
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Inici programa");
+
+            MongoDepartmentImpl mongoImp = new MongoDepartmentImpl();
+
+            List<Department> list = mongoImp.SelectAll();
+
+            foreach (Department department in list)
+            {
+                Console.WriteLine(department.Name);
+            }
         }
     }
 }
